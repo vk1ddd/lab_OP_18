17e815945950ae7a770eceada28ec58a6b88cede
internet <ivanuxas@gmail.com>
2024-05-09 01:43:28 +0300

    replaceWord

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include "string/tasks/removeExtraSpaces.h"
 #include "string/tasks/reverseDigitsWithWords.h"
 #include "string/tasks/replaceDigitsToSpace.h"
+#include "string/tasks/replaceWord.h"
 
 int main(void) {
     tests_removeNonLetters();
     tests_removeExtraSpaces();
     tests_reverseDigitsWithWords();
     tests_replaceDigitsToSpace();
+    tests_replaceWord();
 
     return 0;
 }
